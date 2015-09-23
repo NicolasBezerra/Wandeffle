8ffbb00d00df45d821f68ecad1f79444e0785e0b
Nicolas Bezerra <nicolasbezerra14@gmail.com>
2015-09-23 13:19:22 -0300

    0.2
    - setas correndo com velocidade aleatoria

---
+++ Wandeffle 0.2/Wandeffle/Assets/Assets/Scripts/sobepassarinho.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sobepassarinho : MonoBehaviour {
+
+	public int velocidade;
+		
+	// Use this for initialization
+	void Start () {
+		velocidade = Random.Range (7,10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += new Vector3 (velocidade*Time.deltaTime*-1,0,0);
+	}
+}
