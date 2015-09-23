6411f9d06acfdf01d09ced751620447b13445d3c
Nicolas Bezerra <nicolasbezerra14@gmail.com>
2015-09-23 13:16:36 -0300

    version 0.1
    - Algumas animações
    - controle das setas

---
+++ Wandeffle/Assets/Scripts/ArrowSpawnBehaviour.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ArrowSpawnBehaviour : MonoBehaviour {
+
+	public GameObject [] Arrows;
+	GameObject thisArrow;
+	int random, score;
+	public Text pontos;
+	bool isInstantiated;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		instantiate();
+		 
+		if (GameObject.FindGameObjectWithTag ("up")) {
+			
+			if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) { 
+				DestroyImmediate (GameObject.FindGameObjectWithTag ("up"));
+				isInstantiated = false;
+				scoreAdd();
+			}
+		}
+
+		if (GameObject.FindGameObjectWithTag ("left")) {
+			
+			if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+				DestroyImmediate(GameObject.FindGameObjectWithTag ("left"));
+				isInstantiated = false;
+				scoreAdd();
+			}
+		}
+
+		if (GameObject.FindGameObjectWithTag ("down")) {
+			
+			if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+				DestroyImmediate(GameObject.FindGameObjectWithTag ("down"));
+				isInstantiated = false;
+				scoreAdd();
+			}
+		}
+		
+		if (GameObject.FindGameObjectWithTag ("right")) {
+			
+			if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+				DestroyImmediate(GameObject.FindGameObjectWithTag ("right"));
+				isInstantiated = false;
+				scoreAdd();
+			}
+		}
+	}
+
+	void scoreAdd(){
+		score++;
+		pontos.text = "Score: " + score.ToString();
+	}
+
+	void instantiate()
+	{
+		random = Random.Range (0,4);
+
+		switch (random)
+		{
+			case 0:
+				thisArrow = Arrows[0];
+				break;
+				
+			case 1:
+				thisArrow = Arrows[1];
+				break;
+				
+			case 2:
+				thisArrow = Arrows[2];
+				break;
+				
+			case 3:
+				thisArrow = Arrows[3];
+				break;
+			
+		}
+		
+		if (!isInstantiated) {
+			Instantiate (thisArrow, this.transform.position, Quaternion.identity);
+			isInstantiated = true;
+		}
+	}
+}
---
+++ Wandeffle/Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControll : MonoBehaviour {
+
+	public Animator animator;
+	int direction;
+	float axis;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		axis = Input.GetAxis ("Horizontal");
+
+		if (axis < 0)
+			animator.SetInteger ("direction", 0);//direction = 0;
+		else if(axis > 0)
+			animator.SetInteger ("direction", 1);//direction = 1;
+	}
+}
